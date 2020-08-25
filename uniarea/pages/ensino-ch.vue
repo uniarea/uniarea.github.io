ed635a8b8844f5e1d5fe25aac1abaecec85e46bf
antonioalmeida <theantonioalmeida@gmail.com>
2020-08-25 15:36:50 +0100

    add conditional rendering and input limits

--- uniarea/pages/ensino-ch.vue
+++ uniarea/pages/ensino-ch.vue
@@ -1,15 +1,15 @@
 <template>
   <b-container>
     <h1>Hello World</h1>
-    <b-table :items="items" :fields="fields" >
-           <template v-slot:cell(year_10)="row">
-        <b-form-input v-model="row.item.year_10" type="number"/>
+    <b-table :items="items" :fields="fields">
+      <template v-slot:cell(internal_10)="row">
+        <b-form-input v-model="row.item.internal_10" type="number" min="0" max="20" />
       </template>
-           <template v-slot:cell(year_11)="row">
-        <b-form-input v-model="row.item.year_11" type="number"/>
+      <template v-slot:cell(internal_11)="row">
+        <b-form-input v-model="row.item.internal_11" type="number" min="0" max="20" />
       </template>
-           <template v-slot:cell(year_12)="row">
-        <b-form-input v-model="row.item.year_12" type="number"/>
+      <template v-slot:cell(internal_12)="row">
+        <b-form-input v-if="row.item.internal_12" v-model="row.item.internal_12" type="number" min="0" max="20" />
       </template>
     </b-table>
     {{ items }}
@@ -22,28 +22,28 @@ export default {
     return {
       fields: [
         {
-          key: 'subject',
+          key: 'name',
           label: 'Disciplina'
         },
         {
-          key: 'year_10',
+          key: 'internal_10',
           label: '10º Ano'
         },
         {
-          key: 'year_11',
+          key: 'internal_11',
           label: '11º Ano'
         },
         {
-          key: 'year_12',
+          key: 'internal_12',
           label: '12º Ano'
         }
       ],
       items: [
-        { subject: 'Português', year_10: 10, year_11: 10, year_12: 10 },
-        { subject: 'Filosofia', year_10: 10, year_11: 10, year_12: 10 },
-        { subject: 'Língua Estrangeira', year_10: 10, year_11: 10, year_12: 10 },
-        { subject: 'Educação Física', year_10: 10, year_11: 10, year_12: 10 },
-        { subject: 'Nome Trienal Específica', year_10: 10, year_11: 10, year_12: 10 }
+        { name: 'Português', internal_10: 10, internal_11: 10, internal_12: 10 },
+        { name: 'Filosofia', internal_10: 10, internal_11: 10, internal_12: 10 },
+        { name: 'Língua Estrangeira', internal_10: 10, internal_11: 10, internal_12: 10 },
+        { name: 'Educação Física', internal_10: 10, internal_11: 10, internal_12: null },
+        { name: 'Nome Trienal Específica', internal_10: 10, internal_11: 10, internal_12: 10 }
       ]
     }
   }
